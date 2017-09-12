7167777a3d5f319bd78426ee1f99c225b3858774
biggsbenjamin <bb2515@ic.ac.uk>
2017-09-13 00:08:04 +0100

    ROSNode/robot/skill 	ROSNode/robot/src/rosnode.cpp 	ROSNode/robot/src/rosnode.cpp~ 	ROSNode/robot/src/rosnode.go~ 	ROSNode/robot/src/rosnode.h 	ROSNode/robot/src/rosnode.h~ 	ROSNode/skill.mpk 	hexa info.odt

--- ROSNode/robot/src/rosnode.go
+++ ROSNode/robot/src/rosnode.go
@@ -1,39 +1,74 @@
 package ROSNode
 
+// #include "rosnode.h"
+import "C"
+
 import (
+	"mind/core/framework/log"
 	"mind/core/framework/skill"
+	"time"
+)
+
+//export stringFromC
+func stringFromC() {
+	hi := C.GoString(C.subArray)
+	log.Info.Println(hi)
+}
+
+const (
+	rosMasterIP = "192.168.0.234" // ROS_MASTER_IP need to be modified manually
+	rosSubTopic  = "subTopic"
+	rosPubTopic  = "pubTopic"
+	hello = "HelloWorld"
 )
 
 type ROSNode struct {
 	skill.Base
+	StringNode *C.StringNode
+	stop chan bool
 }
 
 func NewSkill() skill.Interface {
 	// Use this method to create a new skill.
 
-	return &ROSNode{}
+	return &ROSNode{StringNode: C.NewStringNode(C.CString(rosMasterIP), C.CString(rosSubTopic), C.CString(rosPubTopic)),
+		stop: make(chan bool),}
 }
 
 func (d *ROSNode) OnStart() {
 	// Use this method to do something when this skill is starting.
+	d.pubStrings()
 }
 
 func (d *ROSNode) OnClose() {
 	// Use this method to do something when this skill is closing.
+	d.stop <- true
+	C.DeleteStringNode(d.StringNode)
 }
 
-func (d *ROSNode) OnConnect() {
-	// Use this method to do something when the remote connected.
+func (d *ROSNode) pubStrings() {
+	//go d.subStrings()
+	for {
+		select {
+		case <-d.stop:
+			break
+		default:
+		}
+		C.PublishString(
+			d.StringNode,
+			C.CString(hello),
+			C.int(len(hello)),
+		)
+		log.Info.Println("Sent String with length:", len(hello))
+		time.Sleep(1000 * time.Millisecond)
+	}
 }
 
-func (d *ROSNode) OnDisconnect() {
-	// Use this method to do something when the remote disconnected.
-}
+func (d *ROSNode) subStrings(){
+	for {
+		C.SubscribeString(d.StringNode)
 
-func (d *ROSNode) OnRecvJSON(data []byte) {
-	// Use this method to do something when skill receive json data from remote client.
-}
-
-func (d *ROSNode) OnRecvString(data string) {
-	// Use this method to do something when skill receive string from remote client.
+		//log.Info.Println("spun")
+		time.Sleep(1000 * time.Millisecond)
+	}
 }
